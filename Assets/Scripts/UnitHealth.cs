2305bab038e3f68c17f75865dd9a65ede9adcd1d
Mitchell Dunning <ankhman12@gmail.com>
2023-07-09 09:59:07 -0400

    Added barricades system

--- Assets/Scripts/UnitHealth.cs
+++ Assets/Scripts/UnitHealth.cs
@@ -10,16 +10,27 @@ public class UnitHealth : MonoBehaviour
     [SerializeField]
     private Sprite _deadSprite;
 
+    [SerializeField]
+    bool isBarricade = false;
+
     public void TakeDamage(float damage)
     {
         _UnitHP -= damage;
+
         if (_UnitHP < 0)
         {
-            gameObject.GetComponent<SpriteRenderer>().sprite = _deadSprite;
-            GameObject
-                .Find("Particle Manager")
-                .GetComponent<ParticleManager>()
-                .playBlood(transform.position, Quaternion.Euler(0, 0, 90), gameObject);
+            if (isBarricade)
+            {
+                GetComponent<Barricade>().Destruct();
+            }
+            else 
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = _deadSprite;
+                GameObject
+                    .Find("Particle Manager")
+                    .GetComponent<ParticleManager>()
+                    .playBlood(transform.position, Quaternion.Euler(0, 0, 90), gameObject);
+            }
             MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
             foreach (MonoBehaviour c in comps)
             {
