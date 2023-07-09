b2d828c26f534e8ae68bee13c2f80941fe2b22c7
Mitchell Dunning <ankhman12@gmail.com>
2023-07-09 12:29:46 -0400

    Fixed Issues with previous merge

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -16,7 +16,7 @@ public class Castle : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Goblin"))
         {
--- Assets/Scripts/UnitHealth.cs
+++ Assets/Scripts/UnitHealth.cs
@@ -16,7 +16,7 @@ public class UnitHealth : MonoBehaviour
     public bool dead = false;
 
     [SerializeField]
-    bool isBarricade = false;
+    public bool isBarricade = false;
     [SerializeField]
     bool isKing = false;
 
@@ -41,43 +41,11 @@ public class UnitHealth : MonoBehaviour
         {
             if (_UIHealthBar != null)
             {
-                if (_UIHealthBar != null)
-                {
-                    _UIHealthBar.GetComponent<AnimateHP>().ScaleHP(_MaxHP, 0);
-                }
-                if (isKing)
-                {
-                    GameManager.Instance.GameOver(false);
-                }
-                if (isBarricade)
-                {
-                    GetComponent<Barricade>().Destruct();
-                }
-                else
-                {
-                    gameObject.GetComponent<SpriteRenderer>().sprite = _deadSprite;
-                    GameObject
-                        .Find("Particle Manager")
-                        .GetComponent<ParticleManager>()
-                        .playBlood(transform.position, Quaternion.Euler(0, 0, 90), gameObject);
-                }
-                MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
-                foreach (MonoBehaviour c in comps)
-                {
-                    c.enabled = false;
-                }
-                gameObject.tag = "Dead";
-                GetComponent<CapsuleCollider2D>().enabled = false;
-                if (GetComponent<BoxCollider2D>())
-                {
-                    GetComponent<BoxCollider2D>().enabled = false;
-                }
-                GetComponent<SpriteRenderer>().enabled = true;
-                if (GetComponent<Rigidbody2D>() != null)
-                {
-                    GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                    GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                }
+                _UIHealthBar.GetComponent<AnimateHP>().ScaleHP(_MaxHP, 0);
+            }
+            if (isKing)
+            {
+                GameManager.Instance.GameOver(false);
             }
             if (isBarricade)
             {
